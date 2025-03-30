7d1e466ce44b4afe069b35782e2328da54df4123
Lucas C. Abbas <lucasbear9@gmail.com>
2025-03-30 13:44:01 -0800

    Rename setTexturePath to loadTextures for clarity and consistency in MapLoader

--- sunaba/MapLoader.hx
+++ sunaba/MapLoader.hx
@@ -7,7 +7,7 @@ import sunaba.io.IoInterface;
 @:native("gdx.MapLoader")
 extern class MapLoader {
     public static function createMapNode() : Node;
-    public static function setTexturePath(path : String, funcGodotMap : Node, ioInterface : IoInterface) : Void;
+    public static function loadTextures(path : String, ioInterface : IoInterface) : Void;
     public static function loadMap(path : String, funcGodotMap : Node, ioInterface : IoInterface) : Void;
     public static function addPostLoadCallback(func : Function, funcGodotMap : Node) : Void;
 }
