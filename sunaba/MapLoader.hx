5af042e427e7867f953c86f1b0af1004981c1b55
Lucas C. Abbas <lucasbear9@gmail.com>
2025-03-30 13:26:40 -0800

    Add api.hxml configuration and update MapLoader method names for consistency

--- sunaba/MapLoader.hx
+++ sunaba/MapLoader.hx
@@ -8,6 +8,6 @@ import sunaba.io.IoInterface;
 extern class MapLoader {
     public static function createMapNode() : Node;
     public static function setTexturePath(path : String, funcGodotMap : Node, ioInterface : IoInterface) : Void;
-    public static function LoadMap(path : String, funcGodotMap : Node, ioInterface : IoInterface) : Void;
-    public static function AddPostLoadCallback(func : Function, funcGodotMap : Node) : Void;
+    public static function loadMap(path : String, funcGodotMap : Node, ioInterface : IoInterface) : Void;
+    public static function addPostLoadCallback(func : Function, funcGodotMap : Node) : Void;
 }
