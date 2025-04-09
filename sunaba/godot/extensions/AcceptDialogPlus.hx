8a31bd3dc57ad4e826bcd83333ab470577587c3a
Lucas C. Abbas <lucasbear9@gmail.com>
2025-04-08 18:46:20 -0800

    Add AcceptDialogPlus and ConfirmationDialogPlus classes for enhanced dialog functionality

---
+++ sunaba/godot/extensions/AcceptDialogPlus.hx
@@ -0,0 +1,18 @@
+package sunaba.godot.extensions;
+
+import sunaba.godot.AcceptDialog;
+import sunaba.godot.Label;
+
+enum abstract DialogPlusType(Int) {
+    var Error = 0;
+    var Warning = 1;
+    var Info = 2;
+}
+
+@:native("gdx.AcceptDialogPlus")
+extern class AcceptDialogPlus extends AcceptDialog {
+    public var type: DialogPlusType;
+    public var label: Label;
+    public var text: String;
+    public function new(type : DialogPlusType);
+}
---
+++ sunaba/godot/extensions/ConfirmationDialogPlus.hx
@@ -0,0 +1,11 @@
+package sunaba.godot.extensions;
+
+import sunaba.godot.extensions.AcceptDialogPlus.DialogPlusType;
+
+@:native("gdx.ConfirmationDialogPlus")
+extern class ConfirmationDialogPlus {
+    public var type : DialogPlusType;
+    public var label : String;
+    public var text : String;
+    public function new(type : DialogPlusType);
+}
