d67fbd84bdf331d018481e1a319718eaafd2acf7
Lucas C. Abbas <lucasbear9@gmail.com>
2025-04-18 12:19:32 -0800

    Add LuaEvent properties to ScriptNode for enhanced event handling

--- sunaba/godot/extensions/ScriptNode.hx
+++ sunaba/godot/extensions/ScriptNode.hx
@@ -2,6 +2,7 @@ package sunaba.godot.extensions;
 
 import sunaba.godot.Node;
 import sunaba.ScriptBehavior;
+import sunaba.core.LuaEvent;
 
 @:native("gdx.ScriptNode")
 extern class ScriptNode extends Node {
@@ -10,6 +11,18 @@ extern class ScriptNode extends Node {
     public var scriptClass: Class<ScriptBehavior>;
     @:native("classInstance")
     public var instance: ScriptBehavior;
+
+    public var onEnterTree: LuaEvent;
+    public var onReady: LuaEvent;
+    public var onProcess: LuaEvent;
+    public var onPhysicsProcess: LuaEvent;
+    public var onInput: LuaEvent;
+    public var onUnhandledInput: LuaEvent;
+    public var onShortcutInput: LuaEvent;
+    public var onUnhandledKeyInput: LuaEvent;
+    public var onExitTree: LuaEvent;
+    public var onNotification: LuaEvent;
+
     @:native("__new")
     public function new(_cName: String = "");
     public function init(): Void;
