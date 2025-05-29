89eccb1206d52232f1b8d821bc9fa074ba45cc3a
Lucas C. Abbas <lucasbear9@gmail.com>
2025-05-28 16:46:44 -0800

    Add children property to NodeData class

--- sunaba/NodeData.hx
+++ sunaba/NodeData.hx
@@ -6,6 +6,7 @@ class NodeData extends sunaba.core.NativeObject {
     public var type: String;
     public var properties: lua.Table<String, Any>;
     public var parent: Int;
+    public var children: lua.Table<Int, Int>;
 
     @:native("__new")
     public function new();
