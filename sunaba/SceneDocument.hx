cad0caafd6c5bb705c813cd92d1b763a33471d04
Lucas C. Abbas <lucasbear9@gmail.com>
2025-05-28 16:47:59 -0800

    Add createScene function to SceneDocument class for scene creation

--- sunaba/SceneDocument.hx
+++ sunaba/SceneDocument.hx
@@ -1,8 +1,10 @@
 package sunaba;
 
+import sunaba.godot.Node;
 import sunaba.core.NativeObject;
 
 @:native("gdx.SceneDocument")
 extern class SceneDocument extends NativeObject {
     public var nodes: lua.Table<Int, NodeData>;
+    public static function createScene(node : Node): SceneDocument;
 }
