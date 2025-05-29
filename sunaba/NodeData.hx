0ec3307a9146ac0d79329ee498ce4a9997ac8493
Lucas C. Abbas <lucasbear9@gmail.com>
2025-05-28 16:46:26 -0800

    Add parent property to NodeData class

--- sunaba/NodeData.hx
+++ sunaba/NodeData.hx
@@ -5,6 +5,7 @@ class NodeData extends sunaba.core.NativeObject {
     public var id: Int;
     public var type: String;
     public var properties: lua.Table<String, Any>;
+    public var parent: Int;
 
     @:native("__new")
     public function new();
