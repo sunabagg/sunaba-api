993113e20d7136e940e270dd0f329d136d8b25c6
Lucas C. Abbas <lucasbear9@gmail.com>
2025-05-28 16:46:01 -0800

    Add properties field to NodeData class for GDX integration

--- sunaba/NodeData.hx
+++ sunaba/NodeData.hx
@@ -4,6 +4,7 @@ package sunaba;
 class NodeData extends sunaba.core.NativeObject {
     public var id : Int;
     public var type : String;
+    public var properties: lua.Table<String, Any>;
 
     @:native("__new")
     public function new();
