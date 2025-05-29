d6821d7d90b235d1a114dc16bfbd9fb42d913db0
Lucas C. Abbas <lucasbear9@gmail.com>
2025-05-28 16:46:11 -0800

    Fix formatting of variable declarations in NodeData class

--- sunaba/NodeData.hx
+++ sunaba/NodeData.hx
@@ -2,8 +2,8 @@ package sunaba;
 
 @:native("gdx.NodeRef")
 class NodeData extends sunaba.core.NativeObject {
-    public var id : Int;
-    public var type : String;
+    public var id: Int;
+    public var type: String;
     public var properties: lua.Table<String, Any>;
 
     @:native("__new")
