ac4f0faa803dc73d1d82a2fbee6a7e4c32455a51
Lucas C. Abbas <lucasbear9@gmail.com>
2025-05-28 16:45:23 -0800

    Add type property to NodeData class for GDX integration

--- sunaba/NodeData.hx
+++ sunaba/NodeData.hx
@@ -3,6 +3,7 @@ package sunaba;
 @:native("gdx.NodeRef")
 class NodeData extends sunaba.core.NativeObject {
     public var id : Int;
+    public var type : String;
 
     @:native("__new")
     public function new();
