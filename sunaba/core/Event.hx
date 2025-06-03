28148e0f462f4eaf874fb192e5d33521a5d93f3b
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-02 17:22:53 -0800

    Change return type of add and remove methods in Event abstract to Event

--- sunaba/core/Event.hx
+++ sunaba/core/Event.hx
@@ -4,12 +4,14 @@ import haxe.Constraints.Function;
 
 abstract Event(NativeEvent) from NativeEvent to NativeEvent {
     @op(A += B)
-    public function add(func: Function): Void {
+    public function add(func: Function): Event {
         LuaEvent.add(this, func);
+        return this;
     }
 
     @op(A -= B)
-    public function remove(func: Function): Void {
+    public function remove(func: Function): Event {
         LuaEvent.remove(this, func);
+        return this;
     }
 }
