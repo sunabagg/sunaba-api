90d21a6d958ba62256515f0a59c08580bb136619
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-02 17:25:54 -0800

    Add luaDotMethod metadata to add and remove functions in NativeEvent

--- sunaba/core/NativeEvent.hx
+++ sunaba/core/NativeEvent.hx
@@ -3,6 +3,8 @@ package sunaba.core;
 import haxe.Constraints.Function;
 
 extern class NativeEvent {
+    @:luaDotMethod
     public function add(func : Function): Void;
+    @:luaDotMethod
     public function remove(func : Function): Void;
 }
