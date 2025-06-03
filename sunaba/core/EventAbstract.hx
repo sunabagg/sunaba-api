ed0e082e4b0fc13f0aa01d814e2870afbef506a1
Lucas C. Abbas <lucasbear9@gmail.com>
2025-06-02 17:06:38 -0800

    Change parameter type from Function to Any in add and remove methods of EventAbstract

--- sunaba/core/EventAbstract.hx
+++ sunaba/core/EventAbstract.hx
@@ -4,12 +4,12 @@ import haxe.Constraints.Function;
 
 abstract EventAbstract(NativeEvent) from NativeEvent to NativeEvent {
     @op(A += B)
-    public function add(func: Function): Void {
+    public function add(func: Any): Void {
         LuaEvent.add(this, func);
     }
 
     @op(A -= B)
-    public function remove(func: Function): Void {
+    public function remove(func: Any): Void {
         LuaEvent.remove(this, func);
     }
 }
